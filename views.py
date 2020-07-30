b3f05432a59a53337c927c03aa8b4a8174dc11a7
Lucas Salvador <lucas@superclient.com.br>
2020-07-30 20:50:25 -0300

    wip - interface kkk

--- views.py
+++ views.py
@@ -1,24 +1,37 @@
-from tkinter import *
+import wx
+class PDFPanel(wx.Panel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.main_sizer = wx.BoxSizer(wx.VERTICAL)
+        
+        label_file_control = wx.StaticText(self, label='Selecione um modelo (Opcional)')
+        self.main_sizer.Add(label_file_control, 0, wx.ALL | wx.EXPAND, 5)
+        self.file_control = wx.FilePickerCtrl(self)
+        self.main_sizer.Add(self.file_control, 0 , wx.ALL | wx.EXPAND , 5)
 
-janela = Tk()
-janela.title("word-pdf")
-#LxA+E+T
-janela.geometry("800x300")
-janela.mainloop()
+        label_dir_control = wx.StaticText(self, label='Selecione um diretorio')
+        self.main_sizer.Add(label_dir_control, 0, wx.ALL | wx.EXPAND, 5)
+        self.dir_control = wx.DirPickerCtrl(self)
+        self.main_sizer.Add(self.dir_control, 0 , wx.ALL | wx.EXPAND , 5)
+        self.btn_process = wx.Button(self, label="&Processar")
+        self.main_sizer.Add(self.btn_process, 0 , wx.ALL | wx.CENTER , 5)
+        self.SetSizer(self.main_sizer)
+    def initUI():
+        row_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        label = wx.StaticText(self, label='Diretorio',
+                              size=(50, -1))
+        row_sizer.Add(label, 0, wx.ALL, 5)
+        self.file_control = wx.FilePickerCtrl(self)
+        row_sizer.Add(self.file_control, 1, wx.ALL | wx.EXPAND, 5)
+        self.main_sizer.Add(row_sizer, 0, wx.EXPAND)
+class PDFFrame(wx.Frame):
+    def __init__(self):
+        super().__init__(parent=None,
+                        title="PDF-DOCX", size=(500,550))
+        self.panel = PDFPanel(self)
+        self.Show()
 
-# Button(janela, width=20, text="OK", command=funcao)
-# from functools import partial
-# bt1["comando"] = partial(referencia, bt1)
-# Label(janela, text="teste")
-# Entry(janela)
-
-# pack = pacotes
-# side = reserva espaço
-# anchor = alinha
-# fill = faz preenchre no eixo x ou y
-# expand = divide em porções iguais 
-
-# grid =  grid system
-# row e col
-# sticky = alinhamento
-# rowspan e colspan - aumenta linha ou coluna
+if __name__ == '__main__':
+    app = wx.App(False)
+    frame = PDFFrame()
+    app.MainLoop()
